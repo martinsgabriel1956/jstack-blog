9fbe1b873639f674dc3512cbb97372a5dc4138e1
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-18 22:30:34 -0300

    feat: setting the babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1 id="title">
+    <span>Hello World!</span>
+  </h1>
+);
