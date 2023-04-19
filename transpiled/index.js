5bece058d7d525e5b2508949e3577573b018b871
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-18 23:46:26 -0300

    feat: Generating the bundle with Webpack

---
+++ transpiled/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _client = _interopRequireDefault(require("react-dom/client"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_client["default"].createRoot(document.getElementById("root")).render( /*#__PURE__*/_react["default"].createElement("h1", {
+  id: "title"
+}, /*#__PURE__*/_react["default"].createElement("span", null, "Hello World!")));
