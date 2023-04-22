f765b194b2c198d6767773039415a6af0e565359
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-22 12:15:40 -0300

    feat: Creating a local server development

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <h1 id="title">
-    <span>Hello World!!!</span>
+    <span>Hello World!</span>
   </h1>
 );
