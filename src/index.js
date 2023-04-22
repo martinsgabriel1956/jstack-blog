0b8cd0cf97b5daaa79edc4bc0f39e405a40c2b4d
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-22 12:34:14 -0300

    feat: Creating our own first component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 id="title">
-    <span>Hello World!</span>
-  </h1>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
