702d01f5d2eed7158013dce7527a0d94dae2a589
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-22 15:16:11 -0300

    feat: What are the props?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-
-const category = "Daily posts";
+import Post from "./Post";
 
 export default function App() {
   return (
     <>
       <h1>JStack's Blog</h1>
-      <h2>{category}</h2>
+      <h2>Daily posts</h2>
+
+      <hr />
+
+      <Post
+        post={{
+          title: "First New Title",
+          subtitle: "First New Subtitle",
+        }}
+      />
+      <Post
+        post={{
+          title: "Second New Title",
+          subtitle: "Second New Subtitle",
+        }}
+      />
+      <Post
+        post={{
+          title: "Third New Title",
+          subtitle: "Third New Subtitle",
+        }}
+      />
     </>
   );
 }
