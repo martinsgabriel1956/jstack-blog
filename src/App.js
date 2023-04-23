020bf0489caaeb2030a7c5d7aa8713b4af315549
Gabriel Martins <martinsgabriel@gmail.com>
2023-04-22 21:11:31 -0300

    feat: Defining types for the props

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import Post from "./Post";
 export default function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Daily posts</h2>
       </Header>
 
       <hr />
 
       <Post
+        likes={20}
         post={{
           title: "First New Title",
           subtitle: "First New Subtitle",
         }}
       />
       <Post
+        likes={10}
         post={{
           title: "Second New Title",
           subtitle: "Second New Subtitle",
         }}
       />
       <Post
+        likes={50}
         post={{
           title: "Third New Title",
           subtitle: "Third New Subtitle",
