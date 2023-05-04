fff97c0303e05677d0a627da883557a056da1b23
Gabriel Martins <martinsgabriel@gmail.com>
2023-05-03 22:10:14 -0300

    feat: Working with states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Post from "./Post";
 
-const posts = [
+const mockedPosts = [
   {
+    id: Math.random(),
     title: "First New Title",
     subtitle: "First New Subtitle",
     likes: 20,
   },
   {
+    id: Math.random(),
     title: "Second New Title",
     subtitle: "Second New Subtitle",
     likes: 10,
   },
   {
+    id: Math.random(),
     title: "Third New Title",
     subtitle: "Third New Subtitle",
     likes: 50,
@@ -21,10 +24,31 @@ const posts = [
 ];
 
 export default function App() {
+  const [posts, setPosts] = useState(mockedPosts);
+
+  function handleRefresh() {
+    const newMockedPost = (prevState) => {
+      return {
+        id: Math.random(),
+        title: `#${prevState.length + 1} New Title`,
+        subtitle: `#${prevState.length + 1} New Subtitle`,
+        likes: 50,
+      };
+    };
+
+    const updateMockedPosts = (prevState) => [
+      ...prevState,
+      newMockedPost(prevState),
+    ];
+
+    setPosts(updateMockedPosts);
+  }
+
   return (
     <>
       <Header>
         <h2>Daily posts</h2>
+        <button onClick={handleRefresh}>Update</button>
       </Header>
 
       <hr />
