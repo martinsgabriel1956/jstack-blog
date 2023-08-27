3e5fc11870be4889fbaa28dbf083c27cae828640
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-08-26 22:25:50 -0300

    feat: Accessing props

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
-import React, { useContext } from "react";
+import React, { Component } from "react";
 import { Container } from "./styles";
-import { ThemeContext } from "../../contexts/ThemeContext";
 
-export default function Header({ onToggleTheme, selectedTheme }) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button onClick={onToggleTheme}>
-        {selectedTheme === "dark" ? "🌞" : "🌚"}
-      </button>
-    </Container>
-  );
+export default class Header extends Component {
+  render() {
+    const { onToggleTheme, selectedTheme } = this.props;
+
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button onClick={onToggleTheme}>
+          {selectedTheme === "dark" ? "🌞" : "🌚"}
+        </button>
+      </Container>
+    );
+  }
 }
