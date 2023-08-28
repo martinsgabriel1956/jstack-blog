1a4520182427772d595f6ac0fd9a8dda8ad359b2
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-08-28 19:29:37 -0300

    feat: Prop exact and link component

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { PostsList } from "./components/PostList";
 
 export function Routes() {
   return (
-    <BrowserRouter>
-      <Route path="/" component={Home} />
+    <>
+      <Route path="/" exact component={Home} />
       <Route path="/posts" component={PostsList} />
-    </BrowserRouter>
+    </>
   );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
-import PostsList from "../PostList";
+
 import { Routes } from "../../Routes";
+import { Nav } from "./styles";
 
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
