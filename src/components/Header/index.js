522e99df3d1edf38cc7dee44102fb58406935018
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-08-28 12:10:52 -0300

    feat: Tiping the props

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Container } from "./styles";
 
 export default class Header extends Component {
+  static propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+  };
+
   render() {
     const { onToggleTheme, selectedTheme } = this.props;
 
