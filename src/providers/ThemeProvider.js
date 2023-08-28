5d2d8316f1a8255018340cec48aa2c3cc2e3605b
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-08-28 15:16:41 -0300

    feat: Bonus: Saving the theme in the LocalStorage

--- src/providers/ThemeProvider.js
+++ src/providers/ThemeProvider.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export class ThemeProvider extends Component {
-  state = {
-    theme: "dark",
-  };
+  constructor(props) {
+    super(props);
+
+    let theme = "dark";
+
+    try {
+      theme = JSON.parse(localStorage.getItem("theme"));
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.state = {
+      theme,
+    };
+  }
 
   handleToggleTheme = () => {
-    this.setState((prevState) => ({
-      theme: prevState.theme === "dark" ? "light" : "dark",
-    }));
+    const storageTheme = () => {
+      localStorage.setItem("theme", JSON.stringify(this.state.theme));
+    };
+
+    this.setState(
+      (prevState) => ({
+        theme: prevState.theme === "dark" ? "light" : "dark",
+      }),
+      storageTheme
+    );
   };
 
   render() {
