6c405f5b9174f24416bddbfb4a90f0b478189d9f
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-08-29 16:37:59 -0300

    feat: Params and query params in the class components

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -2,12 +2,31 @@ import React, { useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
-export function Post() {
-  const params = useParams();
-  const { search } = useLocation();
-  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+// export function Post() {
+//   const params = useParams();
+//   const { search } = useLocation();
+//   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
 
-  console.debug("Post", params, queryParams.get("meuQueryParam"));
+//   console.debug("Post", params, queryParams.get("meuQueryParam"));
 
-  return <h1>Post</h1>;
+//   return <h1>Post</h1>;
+// }
+
+export class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { search } = this.props.location;
+    this.queryParams = new URLSearchParams(search);
+  }
+
+  render() {
+    console.debug(
+      "Post",
+      this.props.match.params,
+      this.queryParams.get("meuQueryParam")
+    );
+
+    return <h1>Post</h1>;
+  }
 }
